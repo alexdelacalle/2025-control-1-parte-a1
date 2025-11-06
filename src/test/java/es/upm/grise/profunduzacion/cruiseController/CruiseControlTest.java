e9b481bf36bd595e512610bf2442359ef8b2a5d4
Alejandro <alejandro.albaladejo@alumnos.upm.es>
2025-11-06 17:55:06 +0100

    Test completado de CruiseControl

--- src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
+++ src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
@@ -1,5 +1,58 @@
 package es.upm.grise.profunduzacion.cruiseController;
 
-class CruiseControlTest {
-	
+import es.upm.grise.profundizacion.cruiseControl.CruiseControl;
+import es.upm.grise.profundizacion.cruiseControl.Speedometer;
+import es.upm.grise.profundizacion.cruiseControl.IncorrectSpeedSetException;
+import es.upm.grise.profundizacion.cruiseControl.SpeedSetAboveSpeedLimitException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+public class CruiseControlTest {
+
+    private Speedometer speedometer;
+    private CruiseControl cruise;
+
+    @Before
+    public void setUp() {
+        speedometer = Mockito.mock(Speedometer.class);
+        cruise = new CruiseControl(speedometer);
+        cruise.setSpeedLimit(2);
+    }
+
+    @Test(expected = IncorrectSpeedSetException.class)
+    public void testSetSpeedSetThrowsExceptionWhenSpeedIsZero() {
+        cruise.setSpeedSet(0);
+    }
+
+    @Test
+    public void testSetSpeedSetThrowsExceptionWhenSpeedIsNegative() {
+        cruise.setSpeedSet(-3);
+
+    }
+
+    @Test(expected= SpeedSetAboveSpeedLimitException.class)
+    public void testSetSpeedSetThrowsExceptionWhenSpeedLimitIsNull() {
+        cruise.setSpeedLimit(null);
+    	cruise.setSpeedLimit(6);
+    }
+
+    @Test(expected= SpeedSetAboveSpeedLimitException.class)
+    public void testSetSpeedSetThrowsExceptionWhenSpeedSetAboveLimit() {
+        cruise.setSpeedLimit(6);
+
+    }
+
+    @Test
+    public void testSetSpeedSetOkWhenSpeedSetBelowLimit() throws Exception {
+        try {
+            cruise.setSpeedSet(1);
+        }catch(Exception e) {
+        	fail("Ocurrió una excepción inesperada");
+        }
+    }
 }
